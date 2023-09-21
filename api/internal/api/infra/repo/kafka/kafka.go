af8a8b4beedf328786bb41b0000d561b0cda7cba
carlosdarwed <carlosdarwed@gmail.com>
2023-09-21 09:32:10 -0300

    chore(email): provide better logs

--- api/internal/api/infra/repo/kafka/kafka.go
+++ api/internal/api/infra/repo/kafka/kafka.go
@@ -11,22 +11,8 @@ import (
 )
 
 func StartKafkaConsumer(wg *sync.WaitGroup, consumer *kafka.Consumer, topic string) {
-	// // Configurar as opções do consumidor
-	// config := &kafka.ConfigMap{
-	// 	"bootstrap.servers": "localhost:9092", // Substitua pelo(s) endereço(s) do(s) broker(s) Kafka.
-	// 	"group.id":          "de6dd398-fe30-4d47-a6d4-91116e80918b",
-	// 	"auto.offset.reset": "earliest", // Pode ser "earliest" ou "latest" dependendo do comportamento desejado.
-	// }
-
-	// // Crie um consumidor Kafka
-	// consumer, err := kafka.NewConsumer(config)
-	// if err != nil {
-	// 	fmt.Printf("Erro ao criar o consumidor: %v\n", err)
-	// 	return
-	// }
 	defer consumer.Close()
 
-	// Subscreva-se a um tópico
 	err := consumer.SubscribeTopics([]string{topic}, nil)
 	if err != nil {
 		fmt.Printf("Erro ao se inscrever no tópico: %v\n", err)
@@ -35,13 +21,10 @@ func StartKafkaConsumer(wg *sync.WaitGroup, consumer *kafka.Consumer, topic stri
 
 	defer wg.Done()
 
-	// Capture interrupções do sistema
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, os.Interrupt)
 
-	// Aguarde mensagens
-
-	log.Info("kafka consumer connected sucessfully")
+	log.Info("repository: kafka consumer connected sucessfully")
 	run := true
 	for run {
 		select {
